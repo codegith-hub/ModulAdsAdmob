92176e81b66caeb58b63dd002ba4267464be08c4
reskin-aliendroid <rysasahrial@aliendro.id>
2024-07-17 12:32:25 +0700

    add module 2

--- Adsmodul/src/main/java/com/adsmedia/adsmodul/config/SharedPreference3.java
+++ Adsmodul/src/main/java/com/adsmedia/adsmodul/config/SharedPreference3.java
@@ -1,25 +1,28 @@
-package com.adsmedia.adsmodul.model;
+package com.adsmedia.adsmodul.config;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+
+import com.adsmedia.adsmodul.model.DataGlobal3;
 import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class SharedPreference {
+public class SharedPreference3 {
 
     public static final String PREFS_NAME = "PRODUCT_APP";
-    public static final String FAVORITES = "DataGlobal_Favorite";
+    public static final String FAVORITES = "DataGlobal3_Favorite";
 
-    public SharedPreference() {
+    public SharedPreference3() {
         super();
     }
 
     // This four methods are used for maintaining favorites.
-    public void saveFavorites(Context context, List<DataGlobal> favorites) {
+    public void saveFavorites(Context context, List<DataGlobal3> favorites) {
         SharedPreferences settings;
         Editor editor;
 
@@ -35,25 +38,25 @@ public class SharedPreference {
         editor.commit();
     }
 
-    public void addFavorite(Context context, DataGlobal product) {
-        List<DataGlobal> favorites = getFavorites(context);
+    public void addFavorite(Context context, DataGlobal3 product) {
+        List<DataGlobal3> favorites = getFavorites(context);
         if (favorites == null)
             favorites = new ArrayList<>();
         favorites.add(product);
         saveFavorites(context, favorites);
     }
 
-    public void removeFavorite(Context context, DataGlobal product) {
-        ArrayList<DataGlobal> favorites = getFavorites(context);
+    public void removeFavorite(Context context, DataGlobal3 product) {
+        ArrayList<DataGlobal3> favorites = getFavorites(context);
         if (favorites != null) {
             favorites.remove(product);
             saveFavorites(context, favorites);
         }
     }
 
-    public ArrayList<DataGlobal> getFavorites(Context context) {
+    public ArrayList<DataGlobal3> getFavorites(Context context) {
         SharedPreferences settings;
-        List<DataGlobal> favorites;
+        List<DataGlobal3> favorites;
 
         settings = context.getSharedPreferences(PREFS_NAME,
                 Context.MODE_PRIVATE);
@@ -61,14 +64,14 @@ public class SharedPreference {
         if (settings.contains(FAVORITES)) {
             String jsonFavorites = settings.getString(FAVORITES, null);
             Gson gson = new Gson();
-            DataGlobal[] favoriteItems = gson.fromJson(jsonFavorites,
-                    DataGlobal[].class);
+            DataGlobal3[] favoriteItems = gson.fromJson(jsonFavorites,
+                    DataGlobal3[].class);
 
             favorites = Arrays.asList(favoriteItems);
             favorites = new ArrayList<>(favorites);
         } else
             return null;
 
-        return (ArrayList<DataGlobal>) favorites;
+        return (ArrayList<DataGlobal3>) favorites;
     }
 }
